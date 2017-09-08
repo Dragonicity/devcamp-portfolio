dc8473c383fee1f2ca15f6e2c65030da1c72c6ec
Mark-Nelson <mark.nelson@dragonicity.com>
2017-09-08 12:17:06 +0800

    Implemented edit and update for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,4 @@
 class PortfoliosController < ApplicationController
-
   def index
     @portfolio_items = Portfolio.all
   end
@@ -9,7 +8,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :body, :thumb_image))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -20,5 +19,22 @@ class PortfoliosController < ApplicationController
     end
 
   end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :edit }
+      end
+    end
+
+  end
+
 
 end
