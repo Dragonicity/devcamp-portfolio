22c82a480435a87b9b0bcd9d9497491e8a7c6a10
Mark-Nelson <mark.nelson@dragonicity.com>
2017-09-22 12:13:43 +0800

    Add topics sidebar

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'blog'
   def index
     @topics = Topic.all
@@ -12,4 +13,10 @@ class TopicsController < ApplicationController
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
     end
   end
+
+  private
+    def set_sidebar_topics
+      @sidebar_topics = Topic.with_blogs
+      byebug
+    end
 end
